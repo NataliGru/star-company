1c1d25604c4cdfc8fb839a9be296428f0b530d6b
Nataliia Hrushanyk <nataligrushanyk@gmail.com>
2023-10-18 20:27:41 +0300

    added logIn validation

--- src/scripts/logIn.js
+++ src/scripts/logIn.js
@@ -1,12 +1,36 @@
+import {
+  isValidEmail,
+  isValidPassword,
+} from './validation.js';
+
 const logInEmail = document.querySelector('.login-input.email');
 const logInPassword = document.querySelector('.login-input.password');
-const logInButton= document.querySelector('.login-button');
+const logInButton = document.querySelector('.login-button');
+
 const logInError = document.querySelector('.login-email-error');
-
-
-
 
 function showLogInError(errorMessage) {
   logInError.textContent = errorMessage;
-  logInError.style.display = 'block';
+  logInError.style.visibility = 'visible';
 }
+
+function hideLogInError() {
+  logInError.style.visibility = 'hidden';
+}
+
+logInButton.addEventListener('click', () => {
+  if (!isValidEmail(logInEmail.value)) {
+    showLogInError('Please enter a valid email');
+    return false;
+  }
+
+  if (!isValidPassword(logInPassword.value)) {
+    showLogInError('Please enter a valid password');
+    return false;
+  }
+
+  hideLogInError();
+
+  logInEmail.value = '';
+  logInPassword.value = '';
+})
